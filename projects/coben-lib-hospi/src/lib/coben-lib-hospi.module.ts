7f34b108abde01eed61cbd981984c4368a4f4bb6
Tarik Eminagic <tarik.eminagic@cegeka.com>
2019-04-09 17:13:13 +0200

    fix(router): Added hospi route

--- projects/coben-lib-hospi/src/lib/coben-lib-hospi.module.ts
+++ projects/coben-lib-hospi/src/lib/coben-lib-hospi.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CobenLibHospiComponent } from './coben-lib-hospi.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [CobenLibHospiComponent],
   imports: [
+    RouterModule.forChild([
+      { path: 'hospi', component: CobenLibHospiComponent }
+    ])
   ],
   exports: [CobenLibHospiComponent]
 })
